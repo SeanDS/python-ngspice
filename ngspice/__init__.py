9f87803370555353ea89ac734790b08c09628279
Sean Leavey <gpg@attackllama.com>
2022-03-02 17:34:34 +0100

    Update CI

--- ngspice/__init__.py
+++ ngspice/__init__.py
@@ -3,8 +3,8 @@
 # Get package version.
 try:
     from ._version import version as __version__
-except ImportError:
-    raise FileNotFoundError("Could not find _version.py. Ensure you have run setup.")
+except ImportError as err:
+    raise FileNotFoundError("Could not find _version.py. Ensure you have run setup.") from err
 
 from .ngspice import run, run_file, Session, SimulationError
 from .data import Vector, Solution
